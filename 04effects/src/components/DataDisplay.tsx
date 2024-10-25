0279a122a840be8cdd4e495e49c4c37a9399b202
Michal Stehlík <michal.stehlik@pslib.cz>
2024-10-25 09:26:52 +0200

    useEffect

---
+++ 04effects/src/components/DataDisplay.tsx
@@ -0,0 +1,61 @@
+import React, {FC, useState, useEffect} from "react"
+type SubDisplayProps = {
+    value: string;
+}
+
+type Joke = {
+    "icon_url" : string,
+    "id" : string,
+    "url" : string,
+    "value" : string
+}
+
+const SubDisplay: FC<SubDisplayProps> = ({value}) => {
+    useEffect(() => {
+        console.log("Sub Mounted");
+        return () => {
+            console.log("Sub Unmounted");
+        }
+    }, []);
+    console.log("SubDisplay Render");
+    return <p>{value}</p>;
+}
+
+const DataDisplay: FC = () => {
+    const [data, setData] = useState<Joke | null>(null);
+    const [count, setCount] = useState(0);
+    console.log("Render");
+    useEffect(() => {
+        console.log("Effect 0");
+    });
+    useEffect(() => {
+        console.log("Effect 1");
+    }, []);
+    useEffect(() => {
+        console.log("Effect 2");
+    }, [data]);
+    useEffect(() => {
+        const promise = fetch("https://api.chucknorris.io/jokes/random");
+        const res = promise.then(response => response.json());
+        console.log(res);
+        const x = res.then(
+            data => {
+                console.log(data);
+                setData(data);
+            }
+            );
+    }, [count]);
+    console.log("Render 2");
+    return (
+        <>
+            <p>{JSON.stringify(data)}</p>
+            <button onClick={() => setData(null)}>Clear</button>
+            <p>{count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            {data !== null && <SubDisplay value={data.value}/>}
+        </>
+        
+    );
+}
+
+export default DataDisplay;
